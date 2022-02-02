a5513539764c8cbcee01a1d10d5a58353ab77ff0
Ren <cassandra.chen@uoit.net>
2022-02-02 22:47:01 +0000

    authentication required for access to admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['JUNGLE_NAME'],
-                               password: ENV['PASSWORD']
+                               password: ENV['JUNGLE_PASSWORD']
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ProductsController < ApplicationController
   http_basic_authenticate_with name: ENV['JUNGLE_NAME'],
-                               password: ENV['PASSWORD']
+                               password: ENV['JUNGLE_PASSWORD']
   def index
     @products = Product.order(id: :desc).all
   end
